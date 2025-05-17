730cdda02f4c94d60a8a6651f1e8116823382d96
rtkay123 <dev@kanjala.com>
2025-05-17 11:19:42 +0200

    feat: display impl for txtp

--- src/iso20022.rs
+++ src/iso20022.rs
@@ -1,3 +1,22 @@
+#[derive(Debug)]
+pub enum TransactionType {
+    PACS008,
+    PACS002,
+}
+
+impl std::fmt::Display for TransactionType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                TransactionType::PACS002 => "pacs.002.001.12",
+                TransactionType::PACS008 => "pacs.008.001.12",
+            }
+        )
+    }
+}
+
 /// pacs.008.001.12
 pub mod pacs008 {
     tonic::include_proto!("iso20022.pacs008");
